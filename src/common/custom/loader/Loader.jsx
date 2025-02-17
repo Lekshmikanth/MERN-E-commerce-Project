5934d3fec88b7fe4e1a6be1e68d2247203a17b35
Lekshmi-kanth-J <lekshmi.j@trois.in>
2025-02-17 12:48:13 +0530

    feat/BRANCH-1: made changes

---
+++ src/common/custom/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PacmanLoader from "react-spinners";
+
+const Loader = () => {
+    return (
+        <>
+            <PacmanLoader />
+        </>
+    )
+}
+
+export default Loader;
