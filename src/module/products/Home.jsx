a60797e530d5c5a564ea7de04c92953a4ae63419
Lekshmikanth <lekshmikanth1997@gmail.com>
2025-02-22 21:16:40 +0530

    feat/BRANCH-1: changed folder name and routes name

--- src/module/products/Home.jsx
+++ src/module/products/Home.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-function CategoryHome() {
+function ProductsHome() {
     return (
         <div>
             <Outlet />
@@ -8,4 +8,4 @@ function CategoryHome() {
     );
 }
 
-export default CategoryHome;
+export default ProductsHome;
